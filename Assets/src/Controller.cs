9155931076f1e473095f62c68f1cc48776b0b2c0
unknown <j.v.garderen93@gmail.com>
2018-06-06 14:32:48 +0200

    debug log added for touchpoint

--- Assets/src/Controller.cs
+++ Assets/src/Controller.cs
@@ -101,8 +101,10 @@ public class Controller : MonoBehaviour
         //If finger is on touchpad
         if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
         {
+            
             //Read the touchpad values;
             touchpad = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad);
+            Debug.Log(touchpad.x);
             if (touchpad.x < 0) manager.RotateGarmentLeft();
             if (touchpad.x > 0) manager.RotateGarmentRight();
             // Handle movement via touchpad
@@ -127,67 +129,3 @@ public class Controller : MonoBehaviour
         }
     }
 }
-/*
-  * using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Controller : MonoBehaviour {
-
-   // public GameObject selectedOBJ;
-
-
-
-
-    private SteamVR_TrackedController _controller;
-    private PrimitiveType _currentPrimitiveType = PrimitiveType.Sphere;
-    SteamVR_Controller.Device device;
-    SteamVR_TrackedObject controller;
-    Vector2 touchpad;
-    private Manager manager;
-
-    private void OnEnable()
-    {
-        _controller = GetComponent<SteamVR_TrackedController>();
-        _controller.TriggerClicked += HandleTriggerClicked;
-    }
-
-    private void OnDisable()
-    {
-        _controller.TriggerClicked -= HandleTriggerClicked;
-    }
-
-
-
-	// Use this for initialization
-	void Start () {
-        manager = GameObject.Find("Manager").GetComponent<Manager>();
-	}/*
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log(_controller.triggerPressed);
-        if (_controller.triggerPressed)
-        {
-            RaycastHit hit;
-            Ray ray = new Ray(transform.position, transform.forward);
-
-            if (Physics.Raycast(ray, out hit))
-            {
-                GameObject other = hit.collider.gameObject;
-                if (other.tag == "popup")
-                {
-                    Debug.Log("popuphitted");
-                    manager.OpenPopup(hit.transform);
-                }
-            }
-        }
-	}
-
-    private void HandleTriggerClicked(object sender, ClickedEventArgs e)
-    {
-        
-        //manager.checkRay(ray,true);
-    }
-}
-    */
