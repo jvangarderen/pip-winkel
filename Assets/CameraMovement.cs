9c8b4bda5af5a7e901e29fd48588430df3c2c4fc
Pip <pip.balhuizen@hva.nl>
2018-08-01 16:28:36 +0200

    eerroor
    error?

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,15 @@
+
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float panSpeed = 20f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		in (Input.GetKey(
+	}
+}
+
